30b0e767661668c31464e19c2ba09047b0564aa0
Shubham Agrawal <shubham@Shubhams-MacBook-Pro.local>
2021-08-18 23:56:03 +0530

    Working Network

--- env.py
+++ env.py
@@ -7,142 +7,193 @@ from gym.utils import seeding
 import random
 import collections
 from sklearn.feature_extraction.text import CountVectorizer
+import yaml
+import logging
+
+config = None
+
+MAX_WORDLEN = 25
+
+with open("config.yaml", 'r') as stream:
+	try:
+		config = yaml.safe_load(stream)
+	except yaml.YAMLError as exc:
+		print(exc)
+
+logger = logging.getLogger('root')
+# logger.warning('is when this event was logged.')
 
 
 class HangmanEnv(gym.Env):
-    
-    def __init__(self):
-        # super().__init__()
-        
-        self.mistakes_done = 0
-        self.action_space = spaces.Discrete(26)
-        f = open("./words.txt", 'r').readlines()
-        self.wordlist = [w.strip() for w in f]
-        self.vectorizer = CountVectorizer(tokenizer=lambda x: list(x))
-        self.vectorizer2 = CountVectorizer(tokenizer=lambda x: list(x))
-        self.vectorizer2.fit([string.ascii_lowercase])
-        self.vectorizer.fit([string.ascii_lowercase, "_"])
-        self.observation_space = spaces.Tuple((
-            spaces.MultiDiscrete(np.array([25]*27)),
-            spaces.MultiDiscrete(np.array([1]*26)),
-        ))
-        self.observation_space.shape=(27, 26)
-        self.seed()
 
-    def seed(self, seed=None):
-        self.np_random, seed = seeding.np_random(seed)
-        return [seed]
-    
-    def choose_word(self):
-        return random.choice(self.wordlist)
-    
-    def count_words(self, word):
-        lens = [len(w) for w in self.wordlist]
-        counter=dict(collections.Counter(lens))
-        return counter[len(word)]
-    
-    def get_current_worddict(self):
-        return np.array([1 if w in self.word_subset else 0 for w in self.wordlist])
-        
-    def reset(self):
-        self.mistakes_done = 0
-        self.word = self.choose_word()
-        self.wordlen = len(self.word)
-        self.gameover = False
-        self.win = False
-        self.guess_string = "_"*self.wordlen
-        self.actions_used = []
-        self.actions_correct = []
-        
-        print("Word Selected = ", self.word)
-        
-        self.state = (
-            self.vectorizer.transform([self.guess_string]).toarray()[0],
-            np.array([0]*26),       
-        )
-        
-        print("State = ", self.state)
-        return self.state
-        
-    def vec2letter(self, action):
-        letters = string.ascii_lowercase
-        # idx = np.argmax(action==1)
-        return letters[action]
-    
-    def getGuessedWord(self, secretWord, lettersGuessed):
-        """
-        secretWord: string, the word the user is guessing
-        lettersGuessed: list, what letters have been guessed so far
-        returns: string, comprised of letters and underscores that represents
-        what letters in secretWord have been guessed so far.
-        """
-        secretList = []
-        secretString = ''
-        for letter in secretWord:
-            secretList.append(letter)
-        for letter in secretList:
-            if letter not in lettersGuessed:
-                letter = '_'
-            secretString += letter  
-        return secretString
-    
-    
-    def check_guess(self, letter):
-        if letter in self.word:
-            self.prev_string = self.guess_string
-            self.actions_correct.append(letter)
-            self.guess_string = self.getGuessedWord(self.word, self.actions_correct)
-            return True
-        else:
-            return False
-        
-    # def findOccurrences(self, s, ch):
-    #     return [i for i, letter in enumerate(s) if letter == ch]
-        
-    # def evaluate_subset(self, action):
-        
-    #     self.word_subset = [w for w in self.word_subset if self.findOccurrences(self.word, action) == self.findOccurrences(w, action)]
-    #     self.curr_num = len(self.word_subset)
-    #     print("subset length = ", self.num_words)
-    #     print("curr selected =", self.curr_num)
-    
-    def step(self, action):
-        done = False
-        reward = 0
-        if string.ascii_lowercase[action] in self.actions_used:
-            reward = -2
-            self.mistakes_done += 1
-        elif string.ascii_lowercase[action] in self.actions_correct:
-            reward = -5
-        elif self.check_guess(self.vec2letter(action)):
-            print("Correct guess, evaluating reward")
-            if(set(self.word) == set(self.actions_correct)):
-                reward = 10
-                done = True
-                self.win = True
-                self.gameover = True
-            
-            # self.evaluate_subset(action)
-            reward = +1
-            # reward = self.edit_distance(self.state, self.prev_string)
-            self.actions_correct.append(string.ascii_lowercase[action])
-        else:
-            self.mistakes_done += 1
-            if(self.mistakes_done >= 6):
-                reward = -5
-                done = True
-                self.gameover = True
-            else:
-                reward = -2
-        if string.ascii_lowercase[action] not in self.actions_used:
-            self.actions_used.append(string.ascii_lowercase[action])
-        
-        self.state = (
-            self.vectorizer.transform([self.guess_string]).toarray()[0],
-            self.vectorizer2.transform(self.actions_used).toarray()[0] 
-        )
-        return (self.state, reward, done, {'win' :self.win, 'gameover':self.gameover})
-    
-    
-class HangmanPlayer():
-    pass
+	def __init__(self):
+		# super().__init__()
+		self.vocab_size = 26
+		self.mistakes_done = 0
+		f = open("./words.txt", 'r').readlines()
+		self.wordlist = [w.strip() for w in f]
+		self.action_space = spaces.Discrete(26)
+		self.vectorizer = CountVectorizer(tokenizer=lambda x: list(x))
+		# self.wordlist = [w.strip() for w in f]
+		self.vectorizer.fit([string.ascii_lowercase])
+		self.config = config
+		self.char_to_id = {chr(97+x): x for x in range(self.vocab_size)}
+		self.char_to_id['_'] = self.vocab_size
+		self.id_to_char = {v:k for k, v in self.char_to_id.items()}
+		self.observation_space = spaces.Tuple((
+			spaces.MultiDiscrete(np.array([25]*27)),     #Current obscured string
+			spaces.MultiDiscrete(np.array([1]*26))      #Actions used                      #Wordlen
+		))
+		self.observation_space.shape=(27, 26)
+		self.seed()
+
+	def filter_and_encode(self, word, vocab_size, min_len, char_to_id):
+		"""
+		checks if word length is greater than threshold and returns one-hot encoded array along with character sets
+		:param word: word string
+		:param vocab_size: size of vocabulary (26 in this case)
+		:param min_len: word with length less than this is not added to the dataset
+		:param char_to_id
+		"""
+
+		#don't consider words of lengths below a threshold
+		word = word.strip().lower()
+		if len(word) < min_len:
+			return None, None, None
+
+		encoding = np.zeros((len(word), vocab_size + 1))
+		#dict which stores the location at which characters are present
+		#e.g. for 'hello', chars = {'h':[0], 'e':[1], 'l':[2,3], 'o':[4]}
+		chars = {k: [] for k in range(vocab_size+1)}
+
+		for i, c in enumerate(word):
+			idx = char_to_id[c]
+			#update chars dict
+			chars[idx].append(i)
+			#one-hot encode
+			encoding[i][idx] = 1
+
+		zero_vec = np.zeros((MAX_WORDLEN - encoding.shape[0], vocab_size + 1))
+		encoding = np.concatenate((encoding, zero_vec), axis=0)
+
+		return encoding
+
+	def seed(self, seed=None):
+		self.np_random, seed = seeding.np_random(seed)
+		return [seed]
+
+	def choose_word(self):
+		return random.choice(self.wordlist)
+
+	def count_words(self, word):
+		lens = [len(w) for w in self.wordlist]
+		counter=dict(collections.Counter(lens))
+		return counter[len(word)]
+
+	def reset(self):
+		self.mistakes_done = 0
+		# inputs, labels, miss_chars, input_lens, status = self.dataloader.return_batch()
+		self.word = self.choose_word()
+		self.wordlen = len(self.word)
+		self.gameover = False
+		self.win = False
+		self.guess_string = "_"*self.wordlen
+		self.actions_used = set()
+		self.actions_correct = set()
+		logger.info("Reset: Resetting for new word")
+
+		logger.info("Reset: Selected word= {0}".format(self.word))
+
+
+		self.state = (
+			self.filter_and_encode(self.guess_string, 26, 0, self.char_to_id),
+			np.array([0]*26)
+		)
+
+		logger.debug("Reset: Init State = {self.state}")
+
+		return self.state
+
+	def vec2letter(self, action):
+		letters = string.ascii_lowercase
+		# idx = np.argmax(action==1)
+		return letters[action]
+
+	def getGuessedWord(self, secretWord, lettersGuessed):
+		"""
+		secretWord: string, the word the user is guessing
+		lettersGuessed: list, what letters have been guessed so far
+		returns: string, comprised of letters and underscores that represents
+		what letters in secretWord have been guessed so far.
+		"""
+		secretList = []
+		secretString = ''
+		for letter in secretWord:
+			secretList.append(letter)
+		for letter in secretList:
+			if letter not in lettersGuessed:
+				letter = '_'
+			secretString += letter
+		return secretString
+
+
+	def check_guess(self, letter):
+		if letter in self.word:
+			self.prev_string = self.guess_string
+			self.actions_correct.add(letter)
+			self.guess_string = self.getGuessedWord(self.word, self.actions_correct)
+			return True
+		else:
+			return False
+
+	def step(self, action):
+		done = False
+		reward = 0
+		if string.ascii_lowercase[action.argmax()] in self.actions_used:
+			reward = -4.0
+			self.mistakes_done += 1
+			logger.info("Env Step: repeated action, action was= {0}".format(string.ascii_lowercase[action.argmax()]))
+			logger.info("ENV STEP: Mistakes done = {0}".format(self.mistakes_done))
+			if self.mistakes_done >= 6:
+				done = True
+				self.win = True
+				self.gameover = True
+		elif string.ascii_lowercase[action.argmax()] in self.actions_correct:
+			reward = -3.0
+			logger.info("Env Step: repeated correct action, action was= {0}".format(string.ascii_lowercase[action.argmax()]))
+			logger.info("ENV STEP: Mistakes done = {0}".format(self.mistakes_done))
+			# done = True
+			# self.win = True
+			# self.gameover = True
+		elif self.check_guess(self.vec2letter(action.argmax())):
+			logger.info("ENV STEP: Correct guess, evaluating reward, guess was = {0}".format(string.ascii_lowercase[action.argmax()]))
+			if(set(self.word) == self.actions_correct):
+				reward = 10.0
+				done = True
+				self.win = True
+				self.gameover = True
+				logger.info("ENV STEP: Won Game, evaluating reward, guess was = {0}".format(string.ascii_lowercase[action.argmax()]))
+			# self.evaluate_subset(action)
+			reward = +1.0
+			# reward = self.edit_distance(self.state, self.prev_string)
+			self.actions_correct.add(string.ascii_lowercase[action.argmax()])
+		else:
+			logger.info("ENV STEP: Incorrect guess, evaluating reward, guess was = {0}".format(string.ascii_lowercase[action.argmax()]))
+			self.mistakes_done += 1
+			if(self.mistakes_done >= 6):
+				reward = -5.0
+				done = True
+				self.gameover = True
+			else:
+				reward = -2.0
+
+		self.actions_used.add(string.ascii_lowercase[action.argmax()])
+
+		logger.info("ENV STEP: actions used = {0}".format(" ".join(self.actions_used)))
+		logger.info("ENV STEP: actions used = {0}".format(" ".join(self.actions_used)))
+		self.state = (
+			self.filter_and_encode(self.guess_string, 26, 0, self.char_to_id),
+			self.vectorizer.transform(list(self.actions_used)).toarray()[0]
+		)
+		logger.debug("Intermediate State = {self.state}")
+		return (self.state, reward, done, {'win' :self.win, 'gameover':self.gameover})
